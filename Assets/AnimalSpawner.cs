f2c8554d5155e2659adaf8eedd2cce76e8700d60
dhevesy <daniel.hevesy-szettyan@bitmedia.at>
2023-03-01 14:01:23 +0100

    chicken spawn fix

--- Assets/AnimalSpawner.cs
+++ Assets/AnimalSpawner.cs
@@ -7,26 +7,27 @@ public class AnimalSpawner : MonoBehaviour
     public AnimalScript chickenPrefab;
     public cowScript cowPrefab;
     public sheepScript sheepPrefab;
+    public bool isStable = false;
 
     // Start is called before the first frame update
     void Start()
     {
         //Restore Chicken
-        if(SceneLoader.Instance.getChickenState())
+        if(isStable && SceneLoader.Instance.getChickenState())
         {
             Vector2 chicknePos = SceneLoader.Instance.getChickenPos();
             Debug.Log("Init Chicken");
             Instantiate(chickenPrefab.gameObject, chicknePos, Quaternion.identity);
         }
 
-        if(SceneLoader.Instance.cowAlive)
+        if(isStable && SceneLoader.Instance.cowAlive)
         {
             Vector2 cowPos = SceneLoader.Instance.getCowPos();
             Debug.Log("Init Cow");
             Instantiate(cowPrefab.gameObject, cowPos, Quaternion.identity);
         }
 
-        if(SceneLoader.Instance.sheepAlive)
+        if(isStable && SceneLoader.Instance.sheepAlive)
         {
             Vector2 sheepPos = SceneLoader.Instance.getSheepPos();
             Debug.Log("Init Sheep");
